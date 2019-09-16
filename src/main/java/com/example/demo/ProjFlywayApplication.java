ea87159c19f20836188b46da32b21c95c1e324e0
velioterio <velioterio@dxc.com>
2019-09-16 14:42:47 -0300

    Commit do projeto no Flyway DXC

---
+++ src/main/java/com/example/demo/ProjFlywayApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjFlywayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjFlywayApplication.class, args);
+	}
+
+}
